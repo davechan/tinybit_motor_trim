eeb2edfb0a06155657e7bd418012e690c80309b4
Dave Chan <davechan@users.noreply.github.com>
2021-03-22 00:24:57 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,21 @@
+input.onButtonPressed(Button.AB, function () {
+    input.calibrateCompass()
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    base_speed = 50
+    basic.showNumber(degree)
+    for (let index = 0; index <= 10; index++) {
+        Tinybit.CarCtrlSpeed2(Tinybit.CarState.Car_Run, index + base_speed, base_speed)
+        basic.pause(1000)
+        basic.showNumber(degree)
+    }
+})
+let base_speed = 0
+let degree = 0
+degree = input.compassHeading()
 basic.forever(function () {
     Tinybit.CarCtrlSpeed2(Tinybit.CarState.Car_Run, 0, 0)
+    basic.showNumber(degree)
     basic.pause(1000)
     Tinybit.CarCtrlSpeed2(Tinybit.CarState.Car_Run, 55, 50)
     basic.showNumber(5)
